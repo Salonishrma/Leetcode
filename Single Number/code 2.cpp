410de86c45be97e14710d7b872b969a3156eb03f
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-05-01 17:41:04 +0530

    Update code 2.cpp

--- Single Number/code 2.cpp
+++ Single Number/code 2.cpp
@@ -1,6 +1,10 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-}
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+           sum=sum ^ nums[i];
+        }
+        return sum;
+    }
+};
