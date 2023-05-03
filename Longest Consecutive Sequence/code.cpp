2f57796eb8015f09c2601807cdf0f282ebee6d56
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-05-04 00:27:31 +0530

    Update code.cpp

--- Longest Consecutive Sequence/code.cpp
+++ Longest Consecutive Sequence/code.cpp
@@ -1,6 +1,16 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-}
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int size = nums.size();
+        if(!size) return 0;
+        sort(nums.begin(), nums.end());
+        int longest = 0;
+        int cur_longest = 1;
+        for(int i=1;i<size;i++){
+            if(nums[i] == nums[i-1]) continue; //same element as previous skip this
+            else if(nums[i] == nums[i-1] + 1) cur_longest++; //current element is consecutive so increase it.
+            else longest = max(longest,cur_longest), cur_longest = 1; // we can't extend so update longest as the maximum of the both and reset the current_longest.
+        }
+        return max(longest,cur_longest);
+    }
+};
