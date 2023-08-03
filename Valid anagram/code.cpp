aac5a1e083c7abfb14f226b09ca796466e34327e
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-08-03 12:17:51 +0530

    Add files via upload

--- Valid anagram/code.cpp
+++ Valid anagram/code.cpp
@@ -1,43 +1,6 @@
-Method 1
-class Solution {
-public:
-    bool isAnagram(string s, string t) {
-      int n=s.size();
-      int m=t.size();
-      if(m!=n){
-          return false;
-      }
-      map<int,int>m1;
-       map<int,int>m2;
-       for(int i=0;i<n;i++){
-           m1[s[i]]++;
-       }
-       for(int i=0;i<m;i++){
-           m2[t[i]]++;
-       }
-       if(m1!=m2)
-       return false;
-       return true;
-    }
-};
-
-
-Method 2 
-class Solution {
-public:
-    bool isAnagram(string s, string t) {
-      int n=s.size();
-      int m=t.size();
-      if(m!=n){
-          return false;
-      }
-      sort(s.begin(),s.end());
-      sort(t.begin(),t.end());
-      for(int i=0;i<n;i++){
-          if(s[i]!=t[i]){
-              return false;
-          }
-      }
-      return true;
-    }
-};
+{\rtf1\ansi\ansicpg1252\cocoartf2638
+\cocoatextscaling0\cocoaplatform0{\fonttbl}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+}
