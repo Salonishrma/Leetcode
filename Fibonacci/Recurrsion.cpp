53d0458c852a53904ef427476d616f328efffe28
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-08-05 16:08:05 +0530

    Update Recurrsion.cpp

--- Fibonacci/Recurrsion.cpp
+++ Fibonacci/Recurrsion.cpp
@@ -1,6 +1,10 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-}
+class Solution {
+public:
+    int fib(int n) {
+       if(n==0)
+        return 0;
+      if(n==1)
+      return 1;
+      return fib(n-1)+fib(n-2);
+    }
+};
