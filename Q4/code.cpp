e3b9986ac82c159e8a0157c40f7d2b68229a14a4
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-01-05 15:50:30 +0530

    Update code.cpp

--- Q4/code.cpp
+++ Q4/code.cpp
@@ -1,6 +1,43 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2709
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-}
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>v1;
+        double ans;
+        int n=nums1.size();
+        int m=nums2.size();
+        int totlsize=n+m;
+        int i=0;
+        int j=0;
+        while(i<n && j<m){
+                if(nums1[i]<nums2[j])
+                {
+                    v1.push_back(nums1[i]);
+                    i++;
+                }
+                else
+                {
+                    v1.push_back(nums2[j]);
+                    j++;
+                }
+        }
+        while(i<n)
+        {
+            v1.push_back(nums1[i]);
+            i++;
+        }
+        while(j<m)
+        {
+            v1.push_back(nums2[j]);
+            j++;
+        }
+        
+            if(totlsize %2 ==0)
+            {
+               return (double)(v1[totlsize/2]+v1[totlsize/2 -1])/2.0;
+            }
+            else
+            {
+                return (double)(v1[totlsize/2]);
+            }
+    }
+}; 
