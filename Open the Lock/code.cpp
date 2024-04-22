e5383af8e12dd69e0909707da6cb6a547048a6ea
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2024-04-22 18:37:41 +0530

    Create code.cpp

---
+++ Open the Lock/code.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int openLock(vector<string>& deadends, string target) {
+        unordered_set<string>d1(deadends.begin(),deadends.end());
+        if(d1.count("0000")){
+            return -1;
+        }
+        if(target == "0000"){
+            return 0;
+        }
+        int ans =0;
+        queue<string>q{{"0000"}};
+        while(!q.empty()){
+           ++ans;
+           for(int s=q.size();s>0;--s){
+            string word = q.front();
+            q.pop();
+            for(int i=0;i<4;++i){
+                const char cache = word[i];
+                word[i]= word[i]=='9'? '0' :word[i]+1;
+                if(word == target){
+                    return ans;
+                }
+                if(!d1.count(word)){
+                    q.push(word);
+                    d1.insert(word);
+                }
+                word[i]=cache;
+                word[i]= word[i]=='0'? '9' :word[i]-1;
+                if(word == target){
+                    return ans;
+                }
+                if(!d1.count(word)){
+                    q.push(word);
+                    d1.insert(word);
+                }
+                word[i]=cache;
+            }
+           }
+        }
+        return -1;
+    }
+};
